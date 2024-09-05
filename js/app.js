6372bbf061b5657b6a38edfb8aa9798084ace6fd
Jade <jamholmes03@gmail.com>
2024-09-05 13:46:54 -0400

    added javascript file

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const urlBase = 'http://www.knightsthegerbxyz.online/LAMPAPI';
+const extension = 'php';
+
+let loginButton = document.getElementById('login');
+let registerButton = document.getElementById('register');
+
+// login existing users
+function login() {
+    let login = document.getElementById('user').value;
+    let password = document.getElementById('password').value;
+    let loginString = {login:login, password:password};
+    let payload = JSON.stringify(loginString);
+
+    console.log(payload);
+    // ...
+    // implement retrieval of user's contacts info from database
+}
+
+loginButton.addEventListener("click", function(e){
+    login();
+});
