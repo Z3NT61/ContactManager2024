c319df8923345c9be57aa7d69ba67ccab31a4be6
Z3NT61 <zenzedd@gmail.com>
2024-09-10 19:43:08 -0400

    done with login.php need to tweak + sql injection protections, and oauth when time

--- LAMPAPI/login.php
+++ LAMPAPI/login.php
@@ -3,12 +3,40 @@
 #this is funcionality assuming you have already logged in, therefore, this is
 #is just a request get function, returning
 #first name, last name, contacts, and more
+#caching may be a good idea when recieving a users' contact information
+
+$inData = getRequestInfo();
+
+$id = 0;
+$FirstName = "";
+$LastName = "";
+#$Email = "";
+
+$db = new mysqli("localhost", "DBManager", "DesertDesserts45", "SPROJECTDB"); #connects with the DB using the users login n password
+if($db->connect_error){
+    returnWithError($db->connect_error);
+}
+else{
+    $start = $db->prepare("select ID, FirstName, LastName from MAINUSERS where Login=? and Password=?");
+    $start->bind_param("ss",$inData["login"], $inData["password"]);
+    $start->execute();
+    $result = $start->get_result();
+
+    if($row = $result->fetch_assoc()){
+        returnWithInfo($row["FirstName"], $row["LastName"], $row["ID"]);
+    }
+    else{
+        returnWithError("No Records Found");
+    }
+    $start->close();
+    $db->close();
+}
     function getRequestInfo(){
         return json_decode(file_get_contents("php://input"), true);
     }
 
     function sendInfoAsJson($obj){
-        header('Content-type: application/json')
+        header('Content-type: application/json');
         echo $obj;
     }
 
@@ -17,7 +45,7 @@
         sendInfoAsJson($retValue);
     }
 
-function returnWithInfo($FirstName, $LastName){
+function returnWithInfo($FirstName, $LastName, $id){
     #to do
 }
 
