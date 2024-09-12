384f2444ca2e94862978e9ddaeb1cbe2d2e19b92
Z3NT61 <zenzedd@gmail.com>
2024-09-12 19:26:39 -0400

    m

--- js/app.js
+++ js/app.js
@@ -14,6 +14,28 @@ function login() {
     console.log(payload);
     // ...
     // implement retrieval of user's contacts info from database
+    let url = url + "/login" + extension;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try{
+        xhr.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                let jsonObject = JSON.parse(xhr.responseText);
+                userID = jsonObject.id;
+
+                if(userID < 1){ //this user does not exist
+                    //to do
+                    return;
+                }
+            }
+        };
+        xhr.send(payload);
+    }
+    catch(err){
+        //error message
+    }
+
 }
 
 loginButton.addEventListener("click", function(e){
