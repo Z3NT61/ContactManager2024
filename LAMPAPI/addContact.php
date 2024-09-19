6ef8e2fca37046ff231ecd5b99fe85116a94dfdd
Z3NT61 <zenzedd@gmail.com>
2024-09-19 15:34:51 -0400

    finished addContact.php, no testing

---
+++ LAMPAPI/addContact.php
@@ -0,0 +1,57 @@
+<?php
+
+#assuming that dp.php is true and doesn't stop the script,
+#also look into return error info for require, maybe include?
+#see if require can also return data, it can return status.
+
+
+$inData = getRequestInfo();
+
+$id = 0;
+$FirstName = "";
+$LastName = "";
+$Email = "";
+#phone number?
+
+$db = new mysqli("localhost", "root", "b+YXZI98+xeB", "SPROJECTDB"); #connects with the DB using the users login n password
+if($db->connect_error){
+    returnWithError($db->connect_error);
+}
+else{
+    $stmt = $db->prepare("select FirstName, LastName, Email,  count(*) c from CONTACTS group by name having c > 1");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = result->fetch_assoc();
+    $stmt->close();
+    if(!$row["c"] > 1){
+        $start = $db->prepare("insert ID, FirstName, LastName, Email in CONTACTS where FirstName=? and LastName=? and Email=?");
+        $start->bind_param("sss",$inData["login"], $inData["password"], $inData["email"]);
+        $start->execute();
+        $start->close();
+        $db->close();
+    }
+    else{
+        returnWithError("This user already exists");
+    }
+}
+
+
+function getRequestInfo(){
+    return json_decode(file_get_contents("php://input"), true);
+}
+
+function sendInfoAsJson($obj){
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err){
+    $retValue = '{"id":0, "FirstName":"", "LastName":"error", "error": "'. $err . '"}';
+    sendInfoAsJson($retValue);
+}
+
+function returnWithInfo($FirstName, $LastName, $id){
+    #to do
+    $retValue = '{"id":' . $id . ',"FirstName":"'. $FirstName . '","LastName":"'. $LastName . '"}';
+    sendInfoAsJson($retValue);
+}
