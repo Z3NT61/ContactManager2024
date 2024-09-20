5e335b6c3f163cb2b7ba187beb09ec087c7034ac
Z3NT61 <zenzedd@gmail.com>
2024-09-20 12:28:56 -0400

    updated some functions

--- LAMPAPI/addContact.php
+++ LAMPAPI/addContact.php
@@ -18,7 +18,7 @@ if($db->connect_error){
     returnWithError($db->connect_error);
 }
 else{
-    $stmt = $db->prepare("select FirstName, LastName, Email,  count(*) c from CONTACTS group by name having c > 1");
+    $stmt = $db->prepare("select FirstName, LastName, Email,  count(*) c from CONTACTS group by name having c > 1"); #how do we want to filter contacts when added?? Check all fields or just check relationship ie. usercontacts?
     $stmt->execute();
     $result = $stmt->get_result();
     $row = result->fetch_assoc();
@@ -28,6 +28,8 @@ else{
         $start->bind_param("sss",$inData["login"], $inData["password"], $inData["email"]);
         $start->execute();
         $start->close();
+        $start->prepare("insert UserID, ContactID, DataRecordCreated in USERCONTACTS where UserID=? and ContactID=? and DataRecordCreated=?");
+        $start->bind_param("sss", ?, ?); #make sure to get the return ID from $stmt
         $db->close();
         #also need to add the stmt that adds the uid relationship in USERCONTACTS
     }
