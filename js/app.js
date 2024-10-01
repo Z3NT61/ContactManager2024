0416b333ff3dfa0f903b3b0d93f33afc8a49f564
Z3NT61 <zenzedd@gmail.com>
2024-10-01 02:44:35 -0400

    l

--- js/app.js
+++ js/app.js
@@ -163,7 +163,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Open popup when "Add New Contact" button is clicked
   document.getElementById('add').addEventListener('click', openPopup);
   readCookie();
-  document.getElementById('heading').innerHTML = 'Hello,' + firstName + '!';
+  document.getElementById('heading').innerHTML = 'Hello,' + userId + '!';
   // Close popup when the close button is clicked
   document.querySelector('.close-btn').addEventListener('click', closePopup);
 });
