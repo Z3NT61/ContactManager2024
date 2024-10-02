98e42e051ce5d116e3a101e374aff0d4571bbc9e
Jean Carlos <jeancarloslinlai@gmail.com>
2024-10-02 18:31:14 -0400

    wow

--- LAMPAPI/contactList.php
+++ LAMPAPI/contactList.php
@@ -7,23 +7,34 @@ if (!isset($_SESSION['UserID'])) {
 
 $inData = getRequestInfo();
 $userId = $_SESSION['UserID'];
-$searchItem = $inData["searchContactItem"];  // Adjust this based on actual input name
+$searchItem = $inData["searchContactItem"];
 
-$db = new mysqli("localhost", "root", "b+YXZI98+xeB", "SPROJECTDB");
+// It's better to keep credentials out of the code
+require 'config.php'; // Assumes your DB credentials are stored in config.php
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if($db->connect_error) {
     returnWithError($db->connect_error);
 } else {
     $stmt = $db->prepare("SELECT FirstName, LastName, Email, ID FROM CONTACTS WHERE UserID=? AND (FirstName LIKE ? OR LastName LIKE ?)");
+    if (!$stmt) {
+        returnWithError("Failed to prepare the statement");
+        exit();
+    }
+
     $likeSearch = "%{$searchItem}%";
     $stmt->bind_param("sss", $userId, $likeSearch, $likeSearch);
-    $stmt->execute();
+    if (!$stmt->execute()) {
+        returnWithError($stmt->error);
+        exit();
+    }
     $result = $stmt->get_result();
     $contacts = [];
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
         $contacts[] = $row;
     }
-    if(count($contacts) > 0){
-        sendInfoAsJson(json_encode($contacts));
+    if (count($contacts) > 0) {
+        sendInfoAsJson($contacts);
     } else {
         returnWithError("No Contacts Found");
     }
@@ -35,11 +46,10 @@ function getRequestInfo(){
 
 function sendInfoAsJson($obj){
     header('Content-type: application/json');
-    echo $obj;
+    echo json_encode($obj);
 }
 
 function returnWithError($err){
-    $retValue = '{"id":0, "FirstName":"", "LastName":"", "Email":"", "error": "'. $err . '"}';
-    sendInfoAsJson($retValue);
+    sendInfoAsJson(["id" => 0, "FirstName" => "", "LastName" => "", "Email" => "", "error" => $err]);
 }
 ?>
